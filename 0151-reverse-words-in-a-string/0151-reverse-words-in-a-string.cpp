6f8cd9fc5214370c50f1535f339b6f451e44ef87
Keyon Mazandarani <keyonmazandarani@gmail.com>
2025-04-29 22:44:48 -0400

    Time: 0 ms (100%), Space: 10.7 MB (49.22%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left <= right && s[left] == ' ') ++left;
+        while (left <= right && s[right] == ' ') --right;
+
+        deque<string> d;
+        string word;
+
+        while (left <= right) {
+            if (s[left] == ' ' && !word.empty()) {
+                d.push_front(word);
+                word.clear();
+            } else if (s[left] != ' ') {
+                word += s[left];
+            }
+            ++left;
+        }
+        d.push_front(word);
+
+        string result = "";
+        while (!d.empty()) {
+            result += d.front();
+            d.pop_front();
+            if (!d.empty()) {
+                result += " ";
+            }
+        }
+        return result;
+    }
+};
