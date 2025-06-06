1c8892186684f57bacb13f5a7efbbe15ee9d9cb3
Keyon Mazandarani <keyonmazandarani@gmail.com>
2025-06-06 14:37:23 -0400

    Time: 7 ms (72.62%), Space: 20.9 MB (63.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,8 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals.sort(key=lambda x: x[0])
         intervals.sort()
-        
+
         merged = []
         for interval in intervals:
             if not merged or merged[-1][1] < interval[0]:
