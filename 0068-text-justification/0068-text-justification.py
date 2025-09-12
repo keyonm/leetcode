b67bb797c2eb3bb17b0ebe06d575934c5ff93d75
Keyon Mazandarani <keyonmazandarani@gmail.com>
2025-09-12 10:19:50 -0400

    Time: 0 ms (100%), Space: 12.5 MB (14.6%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,44 +1,52 @@
-class Solution:
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        def get_words(i):
-            current_line = []
-            curr_length = 0
-
-            while i < len(words) and curr_length + len(words[i]) <= maxWidth:
-                current_line.append(words[i])
-                curr_length += len(words[i]) + 1
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        
+        """
+        get words, format, add to ans
+        """
+        def get_line(i):
+            line = []
+            length = 0
+            while i < len(words) and length + len(words[i]) <= maxWidth:
+                line.append(words[i])
+                length += len(words[i]) + 1
                 i += 1
-
-            return current_line
+        
+            return line
 
         def create_line(line, i):
-            base_length = -1
+            length = -1
             for word in line:
-                base_length += len(word) + 1
+                length += len(word) + 1
 
-            extra_spaces = maxWidth - base_length
+            spaces = maxWidth - length
 
             if len(line) == 1 or i == len(words):
-                return " ".join(line) + " " * extra_spaces
-
+                return " ".join(line) + " " * spaces
+            
             word_count = len(line) - 1
-            spaces_per_word = extra_spaces // word_count
-            needs_extra_space = extra_spaces % word_count
-
-            for j in range(needs_extra_space):
-                line[j] += " "
+            spaces_per_word = spaces // word_count
 
             for j in range(word_count):
                 line[j] += " " * spaces_per_word
+
+            needs_additional = spaces % word_count
+
+            for j in range(needs_additional):
+                line[j] += " "
 
             return " ".join(line)
 
         ans = []
         i = 0
-
         while i < len(words):
-            current_line = get_words(i)
-            i += len(current_line)
-            ans.append(create_line(current_line, i))
+            curr_words = get_line(i)
+            i += len(curr_words)
+            ans.append(create_line(curr_words, i))
 
         return ans
