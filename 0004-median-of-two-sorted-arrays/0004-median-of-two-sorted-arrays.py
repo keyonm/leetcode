755d4d3b65254467644daac32f83996ff62a6e8c
Keyon Mazandarani <keyonmazandarani@gmail.com>
2025-09-18 12:44:25 -0400

    Time: 3 ms (51.58%), Space: 19.3 MB (5.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,31 +1,32 @@
 class Solution:
-    def findMedianSortedArrays(
-        self, nums1: List[int], nums2: List[int]) -> float:
-        m, n = len(nums1), len(nums2)
-        p1, p2 = 0, 0
+    def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
+        na, nb = len(A), len(B)
+        n = na + nb
 
-        def get_min():
-            nonlocal p1, p2
-            if p1 < m and p2 < n:
-                if nums1[p1] < nums2[p2]:
-                    ans = nums1[p1]
-                    p1 += 1
+        def solve(k, a_start, a_end, b_start, b_end):
+            if a_start > a_end:
+                return B[k - a_start]
+            if b_start > b_end:
+                return A[k - b_start]
+
+            a_index, b_index = (a_start + a_end) // 2, (b_start + b_end) // 2
+            a_value, b_value = A[a_index], B[b_index]
+
+            if a_index + b_index < k:
+                if a_value > b_value:
+                    return solve(k, a_start, a_end, b_index + 1, b_end)
                 else:
-                    ans = nums2[p2]
-                    p2 += 1
-            elif p2 == n:
-                ans = nums1[p1]
-                p1 += 1
+                    return solve(k, a_index + 1, a_end, b_start, b_end)
             else:
-                ans = nums2[p2]
-                p2 += 1
-            return ans
+                if a_value > b_value:
+                    return solve(k, a_start, a_index - 1, b_start, b_end)
+                else:
+                    return solve(k, a_start, a_end, b_start, b_index - 1)
 
-        if (m + n) % 2 == 0:
-            for _ in range((m + n) // 2 - 1):
-                _ = get_min()
-            return (get_min() + get_min()) / 2
+        if n % 2:
+            return solve(n // 2, 0, na - 1, 0, nb - 1)
         else:
-            for _ in range((m + n) // 2):
-                _ = get_min()
-            return get_min()
+            return (
+                solve(n // 2 - 1, 0, na - 1, 0, nb - 1)
+                + solve(n // 2, 0, na - 1, 0, nb - 1)
+            ) / 2
