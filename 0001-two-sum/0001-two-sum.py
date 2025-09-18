5075bc187635b715c5b6b5cce773ba09da703086
Keyon Mazandarani <keyonmazandarani@gmail.com>
2025-09-18 12:05:14 -0400

    Time: 0 ms (100%), Space: 18.9 MB (50.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        map = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for i in range(len(nums)):
-            find = target - nums[i]
-            if find in map:
-                return [i, map[find]]
-            map[nums[i]] = i
+            comp = target - nums[i]
+            if comp in hashmap:
+                return [i, hashmap[comp]]
+            hashmap[nums[i]] = i
+        
         return []
